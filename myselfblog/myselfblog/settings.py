44bf5294a8fe67ef35f45a0a38258132c6a5c303
Madara <Madara>
2023-10-18 17:00:18 +0500

    Добавил Динамические URL и Пользовательские конвертеры

--- myselfblog/myselfblog/settings.py
+++ myselfblog/myselfblog/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+with open("E:\Python courses\secret_key.txt") as secret_key:
+    SECRET_KEY = secret_key.read()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
