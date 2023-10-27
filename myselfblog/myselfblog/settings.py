fd5953bbcc6e0998e803f731ff98cc57e8f9604f
Madara <Madara>
2023-10-27 22:09:30 +0500

    Понятие CRUD. Добавление записей в таблицу БД. добавление некоторых модулей для работы с консолью.

--- myselfblog/myselfblog/settings.py
+++ myselfblog/myselfblog/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myself.apps.MyselfConfig'
+    'django_extensions',
+    'myself.apps.MyselfConfig',
 ]
 
 MIDDLEWARE = [
