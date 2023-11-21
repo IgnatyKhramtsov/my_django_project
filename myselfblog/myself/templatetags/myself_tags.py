e690c478944c69a4f6f9180e5c2fa92f0bbfbb61
Madara <Madara>
2023-11-21 17:01:58 +0500

    Отображение постов по рубрикам

--- myselfblog/myself/templatetags/myself_tags.py
+++ myselfblog/myself/templatetags/myself_tags.py
@@ -1,14 +1,14 @@
 from django import template
 import myself.views as views
-
+from myself.models import Category
 
 register = template.Library()
 
-@register.simple_tag()    # можно указать имен. парам. например name='getcats'
+'''@register.simple_tag()    # можно указать имен. парам. например name='getcats'
 def get_categories():
-    return views.cats_db
+    return views.cats_db'''
 
 @register.inclusion_tag('myself/list_categories.html')
 def show_categories(cat_selected=0):
-    cats = views.cats_db
+    cats = Category.objects.all()
     return {'cats': cats, 'cat_selected': cat_selected}
